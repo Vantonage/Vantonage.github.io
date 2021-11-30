91dcb6c3efef0b6a73e6c3d7d66f8aa4e0acfc82
Vantonage <47269679+Vantonage@users.noreply.github.com>
2021-11-29 21:04:29 -0500

    Working model

---
+++ final.js
@@ -0,0 +1,9 @@
+
+function loadHiring(){
+	if (document.getElementById('hiring').checked){
+		document.getElementById('textBox').style.visibility = 'visible';
+	}
+	else {
+	document.getElementById('textBox').style.visibility = 'hidden';
+	}
+}
