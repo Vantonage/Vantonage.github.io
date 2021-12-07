c0be7cc7c0fc82279013c8c64927e6fa3ecd7060
Vantonage <47269679+Vantonage@users.noreply.github.com>
2021-12-07 18:42:48 -0500

    Mostly working

--- final.js
+++ final.js
@@ -1,9 +1,9 @@
 
 function loadHiring(){
 	if (document.getElementById('hiring').checked){
-		document.getElementById('textBox').style.visibility = 'visible';
+		document.getElementById('textBox').style.display = 'block';
 	}
 	else {
-	document.getElementById('textBox').style.visibility = 'hidden';
+	document.getElementById('textBox').style.display = 'none';
 	}
 }
